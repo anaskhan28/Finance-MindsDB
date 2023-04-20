ba4ab27efc365e765f9b3f2c251e8617096d0072
anaskhan28 <khananasatique@gmail.com>
2023-04-20 16:23:59 +0530

    redux configure 🥲

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App'
 import '@/index.css'
+import {Provider} from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from '@/state/api';
+
+export const store = configureStore({
+  reducer: {[api.reducerPath]: api.reducer},
+  middleware: (getDefualt) => getDefualt().concat(api.middleware),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
 )
