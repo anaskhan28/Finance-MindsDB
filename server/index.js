b8a060cf52bbd2ff28ac7401905eae3653d4f49a
anaskhan28 <khananasatique@gmail.com>
2023-04-22 05:02:52 +0530

    rechart ui with backend data

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import kpiRoutes from './routes/kpi.js'
+import KPI from './models/KPI.js'
+import {kpis} from './data/data.js'
+
 
 // Configurations
 
@@ -18,7 +22,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors());
 
-console.log("hello")
+
+// api routes
+
+app.use("/kpi", kpiRoutes);
 
 // Mongoose Setup
+const PORT = process.env.PORT || 9000;
 
+const mongoConnect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log("Connected to mongoDB!")
+      } catch (error) {
+        console.log(error);
+      }
+    
+      // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis);
+};
+
+app.listen(PORT, () =>{
+    mongoConnect();
+    console.log("Backend server is running!");
+})
